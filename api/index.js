0140f696637f12316a93be6a38f171cc97f270a9
khafidf <khfd027@gmail.com>
2024-11-01 18:29:27 +0700

    second

--- api/index.js
+++ api/index.js
@@ -65,6 +65,10 @@ app.get("/trip", async (req, res) => {
 	}
 });
 
+app.get("/", (req, res) => {
+	res.send("Haloo");
+});
+
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
 });
